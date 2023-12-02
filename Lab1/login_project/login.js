f6570db5a0f44ab149c2446b750e7cc68aed16b1
klisim <wrx75841@gmail.com>
2023-12-02 18:23:44 +0000

    Added comments

--- Lab1/login_project/login.js
+++ Lab1/login_project/login.js
@@ -1,16 +1,26 @@
+// Pobranie referencji do formularza logowania, przycisku logowania i komunikatu o błędzie
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+// Dodanie nasłuchiwacza zdarzeń na przycisku logowania
 loginButton.addEventListener("click", (e) => {
+    // Zatrzymanie domyślnej akcji formularza (przeładowania strony)
     e.preventDefault();
+
+    // Pobranie wartości wprowadzonych przez użytkownika - nazwy użytkownika i hasła
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
+    // Sprawdzenie, czy oba pola formularza są puste
     if (username === "" && password === "") {
+        // Jeśli oba pola są puste, wyświetlenie komunikatu o poprawnym zalogowaniu
         alert("You have successfully logged in.");
+
+        // Przeładowanie strony
         location.reload();
     } else {
+        // Jeśli co najmniej jedno pole nie jest puste, ustawienie nieprzezroczystości komunikatu o błędzie na 1
         loginErrorMsg.style.opacity = 1;
     }
-})
+});
